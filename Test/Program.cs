270f0737eba0496f28fa7d046106049e72b92a3b
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2024-04-22 18:54:31 +0200

    add test files

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace Test {
+    internal class Program {
+        static void Main() {
+            var asm = Assembly.Load("Microsoft.ConfigCI.Commands, Version=10.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+            ConfigCIPatcher.Patcher.Patch();
+            var t = asm.GetType("Microsoft.SecureBoot.UserConfig.VersionInfo");
+            var builder = t.GetMethod("BuildPFNDictionary", BindingFlags.NonPublic | BindingFlags.Static);
+            builder.Invoke(null, null);
+        }
+    }
+}
